ea6848fd28db4f00356c48d9110dd12918bb3a28
Aswankappingal <aswankappingal@gmail.com>
2026-04-23 12:24:08 +0530

    pushed

--- lib/main.dart
+++ lib/main.dart
@@ -49,13 +49,6 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/logo-white.png',
-              width: 250,
-              errorBuilder: (context, error, stackTrace) =>
-                  const Icon(Icons.shopping_bag_outlined, size: 100),
-            ),
-            const SizedBox(height: 48),
             const CircularProgressIndicator(
               color: Colors.black,
             ),
